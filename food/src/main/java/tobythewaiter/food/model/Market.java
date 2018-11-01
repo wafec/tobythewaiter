a6d92582e6da815fd13da211dc80792fffda41f9
Wallace Felipe Cardoso <wallacefcardoso@gmail.com>
2018-10-31 22:22:05 -0300

    TOBY/FOOD: compilation fix

--- food/src/main/java/tobythewaiter/food/model/Market.java
+++ food/src/main/java/tobythewaiter/food/model/Market.java
@@ -2,8 +2,16 @@ package tobythewaiter.food.model;
 
 import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @Data
+@Entity(name="markets")
 public class Market {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     private String name;
 }
